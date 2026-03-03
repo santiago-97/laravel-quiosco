7da9bdae9ecb8d7b7b653b2e23a0479ce24ab8be
Bruce Roberto Santiago Estela Flores <santiago967754641@gmail.com>
2026-03-02 20:05:23 -0500

    actualizar cors para netlify

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://fresh-coffee-santiago-estela.netlify.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
